3a76b47553315be901a9b0999cf2fc838773b6b1
Andrew-0807 <124397787+Andrew-0807@users.noreply.github.com>
2023-02-04 07:09:15 +0200

    #

--- template.cpp
+++ template.cpp
@@ -1,33 +1,40 @@
 ///baisic
-#include <iostream>
-using namespace std;
-int main(){
+#include <bits/stdc++.h>
+using namespace std;\
+
+
+
+int main()
+{
+
+
 
     return 0;
 }
 
 ///vector creation
-void vector_reading(int a[], int n)
+void vector_creation(int a[], int n)
 {
-    for (int i=0;i<n;i++)
-        cin>>a[i];
+    for (int w=0;w<n;w++)
+        cin>>a[w];
 }
 
 ///Vector deletion
 void vector_deletion(int n, int a[], int k)
 {
-    for (int i = k; i< n; i++)
+    for (int w = k; w< n; w++)
     {
-        a[i]=a[i+1];
+        a[w]=a[w+1];
         n--;
     }
 }
 ///Vector inseration
+
 void vector_inseration(int n, int a[], int k);
 {
-    for(int i=n; i>k;i--)
+    for(int w=n; w>k;w--)
     {
-        a[i+1]=a[i];
+        a[w+1]=a[w];
         a[k]=0;
         n++;
     }
@@ -35,10 +42,17 @@ void vector_inseration(int n, int a[], int k);
 ///Vector sorting
 void vector_sorting(int n; int a[];)
 {
-for (int i=0;i<n;i++)
-    for(int j= i++;j<n;j++)
-        if(a[i]>a[j])
-            int aux = a[i];
-            a[i]=a[j];
+for (int w=0;w<n;w++)
+    for(int j= w++;j<n;j++)
+        if(a[w]>a[j])
+            int aux = a[w];
+            a[w]=a[j];
             a[j]= aux;
 }
+
+///vector declaration
+    int n;
+    cin>>n;
+    int v[n];
+    vector_creation(v,n);
+    for(int i=0;i<n;i++)
